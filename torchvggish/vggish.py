b13b1399f34e8cededb38ec4b479859c6569232c
DavidHurst <davidshrst@gmail.com>
2021-09-13 18:24:51 +0100

    Fix import.

--- torchvggish/vggish.py
+++ torchvggish/vggish.py
@@ -1,7 +1,7 @@
-import nn.functional as F
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch import hub
 
 from . import vggish_input, vggish_params
